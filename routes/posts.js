2d76a125cb550f29c34518af0e45d9f0e7faec1e
name name <email@host.com>
2021-02-25 13:25:54 -0500

    chore: simplifying api for demo

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
-router.patch('/:id', updatePost);
+// router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
 
 export default router;
